37f7ef2818ce10ae55b02f456ff701fb5ea7f5dd
Andy Nguyen <xxandynguyenxx@gmail.com>
2016-11-06 22:02:42 -0500

    HW6

---
+++ HW6/HW6/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  HW6
+//
+//  Created by Andy Nguyen on 11/6/16.
+//  Copyright © 2016 Andy Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void printStar(){
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j <= i; j ++){
+            cout << "*";
+        }
+        cout << "" << endl;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    
+    printStar();
+    
+    return 0;
+}
